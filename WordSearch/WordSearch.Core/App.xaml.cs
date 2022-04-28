37409ba4b8d3d96b4cc1851bcaf3a72fedffc8bf
Donat Kolzenov <donat_kolzenov@mail.ru>
2022-04-29 04:05:53 +0700

    Changed file structure of project

--- WordSearch/WordSearch.Core/App.xaml.cs
+++ WordSearch/WordSearch.Core/App.xaml.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
+﻿using Xamarin.Forms;
 
-namespace WordSearch
+namespace WordSearch.Core
 {
     public partial class App : Application
     {
---
+++ WordSearch/WordSearch.Core/ViewModels/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace WordSearch.Core
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}

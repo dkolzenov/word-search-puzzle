b388f6eafb64e160b39441e5f868bf1ae4db5c06
Donat Kolzenov <donat_kolzenov@mail.ru>
2022-06-19 02:26:12 +0700

    add: Presentation project

---
+++ src/Common/WordSearch.Presentation/ViewModels/Base/BaseViewModel.cs
@@ -0,0 +1,41 @@
+﻿namespace WordSearch.Presentation.ViewModels.Base
+{
+    using Prism.Mvvm;
+    using Prism.Navigation;
+
+    public abstract class BaseViewModel : BindableBase, INavigationAware, IDestructible
+    {
+        private string _title = null!;
+
+        public string Title
+        {
+            get => _title;
+            set => SetProperty(ref _title, value);
+        }
+
+        protected INavigationService NavigationService { get; }
+
+        public BaseViewModel(INavigationService navigationService)
+        {
+            NavigationService = navigationService;
+        }
+
+        // INavigationAware
+        public virtual void OnNavigatedFrom(INavigationParameters parameters)
+        {
+
+        }
+
+        // INavigationAware
+        public virtual void OnNavigatedTo(INavigationParameters parameters)
+        {
+
+        }
+
+        // IDestructible
+        public virtual void Destroy()
+        {
+
+        }
+    }
+}
---
+++ src/Common/WordSearch.Presentation/ViewModels/MainPage/MainPageViewModel.cs
@@ -0,0 +1,46 @@
+﻿namespace WordSearch.Presentation.ViewModels.MainPage
+{
+    using System.Collections.Generic;
+
+    using Prism.Navigation;
+    using Prism.Commands;
+
+    using WordSearch.Presentation.ViewModels.Base;
+    using WordSearch.Services.Word.Enums;
+    using WordSearch.Services.Interfaces;
+
+    public class MainPageViewModel : BaseViewModel
+    {
+        private List<string> _words = null!;
+
+        private readonly IWordService _wordService = null!;
+
+        public List<string> Words
+        {
+            get => _words;
+
+            set => SetProperty(ref _words, value);
+        }
+
+        public DelegateCommand GetAllWordsCommand { get; set; }
+
+        public MainPageViewModel(INavigationService navigationService)
+            : base(navigationService)
+        {
+            Title = "Main Page";
+            GetAllWordsCommand = new DelegateCommand(GetAllWords);
+        }
+
+        public MainPageViewModel(INavigationService navigationService, IWordService wordService)
+            : this(navigationService)
+        {
+            Words = new List<string>();
+            _wordService = wordService;
+        }
+
+        private async void GetAllWords()
+        {
+            Words = await _wordService.GetWords(LanguageType.Russian, CategoryType.Animals);
+        }
+    }
+}

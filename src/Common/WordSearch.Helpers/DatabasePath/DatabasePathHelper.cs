3daeae7a10f58cbb44f6249b10ac9157fa27f902
Donat Kolzenov <donat_kolzenov@mail.ru>
2022-06-19 02:25:13 +0700

    add: Helpers project

---
+++ src/Common/WordSearch.Helpers/DatabasePath/DatabasePathHelper.cs
@@ -0,0 +1,17 @@
+﻿namespace WordSearch.Helpers.DatabasePath
+{
+    using System;
+    using System.IO;
+
+    using WordSearch.Helpers.Interfaces;
+
+    public class DatabasePathHelper : IDatabasePathHelper
+    {
+        public string GetDatabasePath(string databaseName)
+        {
+            return Path.Combine(Environment.GetFolderPath(
+                Environment.SpecialFolder.LocalApplicationData),
+                databaseName);
+        }
+    }
+}

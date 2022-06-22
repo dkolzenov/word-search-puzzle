c394d287dc4b64bfe8521eedb4ea6b042c43d60a
Donat Kolzenov <donat_kolzenov@mail.ru>
2022-06-22 20:35:56 +0700

    update: Bootstrapper structure

---
+++ src/Application/WordSearch.Bootstrapper/Core/PrismBootstrapper.cs
@@ -0,0 +1,47 @@
+﻿namespace WordSearch.Bootstrapper.Core
+{
+    using System.Reflection;
+
+    using Prism.Ioc;
+    using Prism.DryIoc;
+    using Prism.Modularity;
+
+    using WordSearch.Ioc.Modules;
+    using WordSearch.Data.Properties;
+    using WordSearch.Data.Database.Constants;
+    using WordSearch.Helpers.Interfaces;
+
+    internal partial class PrismBootstrapper : PrismApplication
+    {
+        protected override void OnInitialized()
+        {
+            InitializeDatabases();
+        }
+
+        protected override void ConfigureModuleCatalog(
+            IModuleCatalog moduleCatalog)
+        {
+            moduleCatalog.AddModule<IocModule>();
+        }
+
+        protected override void RegisterTypes(
+            IContainerRegistry containerRegistry)
+        {
+        }
+
+        private void InitializeDatabases()
+        {
+            var resourceWriter = App.Current.Container.
+                Resolve<IResourceWriterHelper>(
+                (typeof(Assembly), _ = DataProperties.Assembly));
+
+            resourceWriter.Write(
+                WordsDbConstants.ResourceNamespace,
+                WordsDbConstants.Name);
+
+            resourceWriter.Write(
+                CharactersDbConstants.ResourceNamespace,
+                CharactersDbConstants.Name);
+        }
+    }
+}

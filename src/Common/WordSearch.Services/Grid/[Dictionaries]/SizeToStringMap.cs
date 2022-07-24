c59c9b35428581afd625170b2d4a3839705aa242
Donat Kolzenov <donat_kolzenov@mail.ru>
2022-07-24 23:47:09 +0900

    add: grid service

---
+++ src/Common/WordSearch.Services/Grid/[Dictionaries]/SizeToStringMap.cs
@@ -0,0 +1,31 @@
+﻿namespace WordSearch.Services.Grid.Dictionaries
+{
+    using System.Collections.Generic;
+
+    using WordSearch.Services.Grid.Enums;
+
+    public class SizeToStringMap
+    {
+        private readonly Dictionary<SizeType, string> _map;
+
+        public SizeToStringMap()
+        {
+            _map = new Dictionary<SizeType, string>()
+            {
+                {SizeType.Small, "Маленький"},
+                {SizeType.Small, "Small"},
+
+                {SizeType.Medium, "Средний"},
+                {SizeType.Medium, "Medium"},
+
+                {SizeType.Large, "Большой"},
+                {SizeType.Large, "Large"},
+            };
+        }
+
+        public string GetItemString(SizeType size)
+        {
+            return _map[size];
+        }
+    }
+}

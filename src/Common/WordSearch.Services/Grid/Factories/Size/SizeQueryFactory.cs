ecead1baae4350c93556873e6036f28da2cd668a
Donat Kolzenov <donat_kolzenov@mail.ru>
2022-07-25 00:48:12 +0900

    add: grid factories

---
+++ src/Common/WordSearch.Services/Grid/Factories/Size/SizeQueryFactory.cs
@@ -0,0 +1,33 @@
+﻿namespace WordSearch.Services.Grid.Factories.Size
+{
+    using System;
+    using System.Linq.Expressions;
+
+    using WordSearch.Services.Grid.Enums;
+    using WordSearch.Services.Grid.Factories.Interfaces;
+    using WordSearch.Data.Entities.Grid;
+
+    public class SizeQueryFactory : ISizeQueryFactory
+    {
+        public Expression<Func<GridEntity, bool>> CreateSmallQuery()
+        {
+            var size = SizeType.Small.ToString().ToLower();
+
+            return Grid => Grid.Size == size;
+        }
+
+        public Expression<Func<GridEntity, bool>> CreateMediumQuery()
+        {
+            var size = SizeType.Medium.ToString().ToLower();
+
+            return Grid => Grid.Size == size;
+        }
+
+        public Expression<Func<GridEntity, bool>> CreateLargeQuery()
+        {
+            var size = SizeType.Large.ToString().ToLower();
+
+            return Grid => Grid.Size == size;
+        }
+    }
+}
---
+++ src/Common/WordSearch.Services/Grid/Factories/[Interfaces]/IGridQueryFactory.cs
@@ -0,0 +1,13 @@
+﻿namespace WordSearch.Services.Grid.Factories.Interfaces
+{
+    using System;
+    using System.Linq.Expressions;
+
+    using WordSearch.Data.Entities.Grid;
+    using WordSearch.Services.Grid.Enums;
+
+    public interface IGridQueryFactory
+    {
+        Expression<Func<GridEntity, bool>> CreateSizeQuery(SizeType sizeType);
+    }
+}

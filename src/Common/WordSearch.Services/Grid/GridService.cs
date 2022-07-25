9732ea037284ce94326b5212e5474c7697429fa5
Donat Kolzenov <donat_kolzenov@mail.ru>
2022-07-25 14:55:58 +0900

    update: grid service

--- src/Common/WordSearch.Services/Grid/GridService.cs
+++ src/Common/WordSearch.Services/Grid/GridService.cs
@@ -4,17 +4,42 @@
     using System.Threading.Tasks;
 
     using WordSearch.Services.Interfaces;
+    using WordSearch.Services.Grid.Factories.Interfaces;
     using WordSearch.Services.Grid.Enums;
+    using WordSearch.Data.Repositories.Interfaces;
+    using WordSearch.Data.Entities.Grid;
 
     public class GridService : IGridService
     {
-        public GridService()
+        private readonly IGridQueryFactory _gridFactory;
+
+        private readonly IGridRepository _gridRepository;
+
+        public GridService(
+            IGridQueryFactory gridFactory,
+            IGridRepository gridRepository)
         {
+            _gridFactory = gridFactory;
+            _gridRepository = gridRepository;
         }
 
-        public Task<Array> GetGrid(SizeType size)
+        public async Task<GridEntity> GetGrid(SizeType sizeType)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var sizeQuery = _gridFactory.CreateSizeQuery(sizeType);
+
+                var result = await _gridRepository.QueryAsync(sizeQuery);
+
+                var grid = result.GetEnumerator().Current;
+
+                return grid;
+            }
+            catch (Exception ex)
+            {
+                return await Task.FromException<GridEntity>(
+                    ex.InnerException);
+            }
         }
     }
 }

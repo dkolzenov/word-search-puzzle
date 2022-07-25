a509233e8a0af199beaa5004fa1c4518c8e729b5
Donat Kolzenov <donat_kolzenov@mail.ru>
2022-07-25 16:23:06 +0900

    add: mapper profile

---
+++ src/Common/WordSearch.Mappers/Profile/MappingProfile.cs
@@ -0,0 +1,33 @@
+﻿namespace WordSearch.Mappers.Profile
+{
+    using AutoMapper;
+
+    using WordSearch.Data.Entities.Character;
+    using WordSearch.Data.Entities.Grid;
+    using WordSearch.Data.Entities.Word;
+    using WordSearch.Models.Character;
+    using WordSearch.Models.Grid;
+    using WordSearch.Models.Word;
+
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            #region Entities to Models
+
+            CreateMap<CharacterEntity, CharacterModel>();
+            CreateMap<GridEntity, GridModel>();
+            CreateMap<WordEntity, WordModel>();
+
+            #endregion
+
+            #region Models to Entities
+
+            CreateMap<CharacterModel, CharacterEntity>();
+            CreateMap<GridModel, GridEntity>();
+            CreateMap<WordModel, WordEntity>();
+
+            #endregion
+        }
+    }
+}

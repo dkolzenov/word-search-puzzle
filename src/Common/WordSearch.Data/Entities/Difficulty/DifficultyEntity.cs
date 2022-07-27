8fe02d933e495a31eb48febda6e2a16b1dc71886
Donat Kolzenov <donat_kolzenov@mail.ru>
2022-07-27 17:33:52 +0900

    add: difficulty entity

---
+++ src/Common/WordSearch.Data/Entities/Difficulty/DifficultyEntity.cs
@@ -0,0 +1,22 @@
+﻿namespace WordSearch.Data.Entities.Difficulty
+{
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    [Table("Difficulties")]
+    public class DifficultyEntity
+    {
+        public int Id { get; set; }
+
+        public string Value { get; set; } = null!;
+
+        public string GridSize { get; set; } = null!;
+
+        public int WordCount { get; set; }
+
+        // TODO: Future updates
+
+        //public int TimeLimit { get; set; }
+
+        //public int ScoreMultiplier { get; set; }
+    }
+}

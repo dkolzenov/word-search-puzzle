469fde6ff4d3c783c15a506ef532e6644b844527
Donat Kolzenov <donat_kolzenov@mail.ru>
2022-07-28 00:00:08 +0900

    add: game settings entity

---
+++ src/Common/WordSearch.Data/Entities/GameSettings/GameSettingsEntity.cs
@@ -0,0 +1,21 @@
+﻿namespace WordSearch.Data.Entities.GameSettings
+{
+    public class GameSettingsEntity
+    {
+        public int Id { get; set; }
+
+        public string Difficulty { get; set; } = null!;
+
+        public string GridSize { get; set; } = null!;
+
+        public int WordCount { get; set; }
+
+        public int MaxWordLength { get; set; }
+
+        // TODO: Future updates
+
+        //public int TimeLimit { get; set; }
+
+        //public int ScoreMultiplier { get; set; }
+    }
+}

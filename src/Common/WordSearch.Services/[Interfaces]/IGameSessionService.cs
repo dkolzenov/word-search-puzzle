736699d66d596623ca7e8fbe52d04ed7e1145e7f
Donat Kolzenov <donat_kolzenov@mail.ru>
2022-07-27 16:33:08 +0900

    add: game session service

---
+++ src/Common/WordSearch.Services/[Interfaces]/IGameSessionService.cs
@@ -0,0 +1,13 @@
+﻿namespace WordSearch.Services.Interfaces
+{
+    using System.Threading.Tasks;
+
+    using WordSearch.Models.GameSettings;
+
+    public interface IGameSessionService
+    {
+        Task<bool> StartSession(GameSettingsModel model);
+
+        Task<bool> EndSession();
+    }
+}

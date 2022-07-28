eaa0656d87cf0932a0eefafa12dd2cefe8c8150d
Donat Kolzenov <donat_kolzenov@mail.ru>
2022-07-28 15:53:20 +0900

    update: grid dictionary joined to work

--- src/Common/WordSearch.Services/Grid/Factories/GridQueryFactory.cs
+++ src/Common/WordSearch.Services/Grid/Factories/GridQueryFactory.cs
@@ -4,15 +4,23 @@
     using System.Linq.Expressions;
 
     using WordSearch.Services.Grid.Enums;
+    using WordSearch.Services.Grid.Dictionaries;
     using WordSearch.Services.Grid.Factories.Interfaces;
     using WordSearch.Data.Entities.Grid;
 
     public class GridQueryFactory : IGridQueryFactory
     {
+        private readonly SizeToStringMap _sizeMap;
+
+        public GridQueryFactory()
+        {
+            _sizeMap = new SizeToStringMap();
+        }
+
         public Expression<Func<GridEntity, bool>> CreateSizeQuery(
             SizeType sizeType)
         {
-            var size = sizeType.ToString().ToLower();
+            string size = _sizeMap.GetSizeString(sizeType);
 
             return Grid => Grid.Size == size;
         }

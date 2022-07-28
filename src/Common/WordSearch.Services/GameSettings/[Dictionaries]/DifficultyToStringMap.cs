fbe0de76693ce4dea8f54ecae1ecb0399decb3d9
Donat Kolzenov <donat_kolzenov@mail.ru>
2022-07-28 11:43:12 +0900

    add: game settings service

---
+++ src/Common/WordSearch.Services/GameSettings/[Dictionaries]/DifficultyToStringMap.cs
@@ -0,0 +1,31 @@
+﻿namespace WordSearch.Services.GameSettings.Dictionaries
+{
+    using System.Collections.Generic;
+
+    using WordSearch.Services.GameSettings.Enums;
+
+    public class DifficultyToStringMap
+    {
+        private readonly Dictionary<DifficultyType, string> _map;
+
+        public DifficultyToStringMap()
+        {
+            _map = new Dictionary<DifficultyType, string>()
+            {
+                {DifficultyType.Easy, "Легко"},
+                {DifficultyType.Easy, "Easy"},
+
+                {DifficultyType.Normal, "Нормально"},
+                {DifficultyType.Normal, "Normal"},
+
+                {DifficultyType.Hard, "Тяжело"},
+                {DifficultyType.Hard, "Hard"},
+            };
+        }
+
+        public string GetItemString(DifficultyType difficultyType)
+        {
+            return _map[difficultyType];
+        }
+    }
+}
---
+++ src/Common/WordSearch.Services/[Interfaces]/IGameSettingsService.cs
@@ -0,0 +1,12 @@
+﻿namespace WordSearch.Services.Interfaces
+{
+    using System.Threading.Tasks;
+
+    using WordSearch.Services.GameSettings.Enums;
+    using WordSearch.Models.GameSettings;
+
+    public interface IGameSettingsService
+    {
+        Task<GameSettingsModel> GetSettings(DifficultyType difficultyType);
+    }
+}

6bc6dec789cb198e496406c36c40db396a233005
Donat Kolzenov <donat_kolzenov@mail.ru>
2022-07-28 10:43:58 +0900

    add: game settings repository

---
+++ src/Common/WordSearch.Data/Repositories/GameSettings/GameSettingsRepository.cs
@@ -0,0 +1,16 @@
+﻿namespace WordSearch.Data.Repositories.GameSettings
+{
+    using WordSearch.Data.Repositories.Interfaces;
+    using WordSearch.Data.Repositories.Base;
+    using WordSearch.Data.Entities.GameSettings;
+    using WordSearch.Data.Contexts.ApplicationDb;
+
+    public class GameSettingsRepository
+        : RepositoryBase<GameSettingsEntity>, IGameSettingsRepository
+    {
+        public GameSettingsRepository(ApplicationDbContext context)
+            : base(context)
+        {
+        }
+    }
+}

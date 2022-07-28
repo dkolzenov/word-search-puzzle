ca03bd2d1dda600b975102598171c09345ca72de
Donat Kolzenov <donat_kolzenov@mail.ru>
2022-07-28 16:10:04 +0900

    add: game settings query factory

---
+++ src/Common/WordSearch.Services/GameSettings/Factories/GameSettingsQueryFactory.cs
@@ -0,0 +1,29 @@
+﻿namespace WordSearch.Services.GameSettings.Factories
+{
+    using System;
+    using System.Linq.Expressions;
+
+    using WordSearch.Services.GameSettings.Enums;
+    using WordSearch.Services.GameSettings.Dictionaries;
+    using WordSearch.Services.GameSettings.Factories.Interfaces;
+    using WordSearch.Data.Entities.GameSettings;
+
+    public class GameSettingsQueryFactory : IGameSettingsQueryFactory
+    {
+        private readonly DifficultyToStringMap _difficultyMap;
+
+        public GameSettingsQueryFactory()
+        {
+            _difficultyMap = new DifficultyToStringMap();
+        }
+
+        public Expression<Func<GameSettingsEntity, bool>> CreateDifficultyQuery(
+            DifficultyType difficultyType)
+        {
+            string difficulty = _difficultyMap
+                .GetDifficultyString(difficultyType);
+
+            return Settings => Settings.Difficulty == difficulty;
+        }
+    }
+}
--- src/Common/WordSearch.Services/GameSettings/GameSettingsService.cs
+++ src/Common/WordSearch.Services/GameSettings/GameSettingsService.cs
@@ -6,7 +6,7 @@
     using AutoMapper;
 
     using WordSearch.Services.Interfaces;
-    //using WordSearch.Services.GameSettings.Factories.Interfaces;
+    using WordSearch.Services.GameSettings.Factories.Interfaces;
     using WordSearch.Services.GameSettings.Enums;
     using WordSearch.Data.Repositories.Interfaces;
     using WordSearch.Models.GameSettings;
@@ -15,17 +15,17 @@
     {
         private readonly IMapper _mapper;
 
-        //private readonly IGameSettingsQueryFactory _gameSettingsFactory;
+        private readonly IGameSettingsQueryFactory _gameSettingsFactory;
 
         private readonly IGameSettingsRepository _gameSettingsRepository;
 
         public GameSettingsService(
             IMapper mapper,
-            //IGameSettingsQueryFactory gameSettingsFactory,
+            IGameSettingsQueryFactory gameSettingsFactory,
             IGameSettingsRepository gameSettingsRepository)
         {
             _mapper = mapper;
-            //_gameSettingsFactory = gameSettingsFactory;
+            _gameSettingsFactory = gameSettingsFactory;
             _gameSettingsRepository = gameSettingsRepository;
         }
 

c7bd233ac81b87acf11f657d53621654f7388958
Donat Kolzenov <donat_kolzenov@mail.ru>
2022-07-28 10:18:47 +0900

    add: game settings context

--- src/Common/WordSearch.Data/Contexts/ApplicationDb/ApplicationDbContext.cs
+++ src/Common/WordSearch.Data/Contexts/ApplicationDb/ApplicationDbContext.cs
@@ -5,6 +5,7 @@
     using WordSearch.Data.Entities.Word;
     using WordSearch.Data.Entities.Character;
     using WordSearch.Data.Entities.Grid;
+    using WordSearch.Data.Entities.GameSettings;
 
     public class ApplicationDbContext : DbContext
     {
@@ -13,6 +14,8 @@
         public DbSet<CharacterEntity> Characters { get; set; } = null!;
 
         public DbSet<GridEntity> Grids { get; set; } = null!;
+
+        public DbSet<GameSettingsEntity> GameSettings { get; set; } = null!;
 
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)

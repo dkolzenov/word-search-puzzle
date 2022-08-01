48e72e61d352e1e764323bcb1b4638afe75b599b
Donat Kolzenov <donat_kolzenov@mail.ru>
2022-08-01 20:56:19 +0900

    add: vertical top-to-bottom word direction

---
+++ src/Common/WordSearch.Services/GridDataPlacement/WordDirection/Vertical/TopToBottom.cs
@@ -0,0 +1,13 @@
+﻿namespace WordSearch.Services.GridDataPlacement.WordDirection.Vertical
+{
+    using WordSearch.Services.GridDataPlacement.WordDirection.Interfaces;
+
+    public class TopToBottom : IWordDirection
+    {
+        public void MoveInGrid(ref int row, ref int column)
+        {
+            row += MovementState.ShiftOneCell;
+            column += MovementState.None;
+        }
+    }
+}

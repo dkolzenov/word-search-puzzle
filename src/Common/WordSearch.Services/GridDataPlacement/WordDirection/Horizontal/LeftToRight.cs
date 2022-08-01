9582aa0736eeb3a4922d31277b05e342335ed821
Donat Kolzenov <donat_kolzenov@mail.ru>
2022-08-01 20:37:24 +0900

    add: horizontal left-to-right word direction

---
+++ src/Common/WordSearch.Services/GridDataPlacement/WordDirection/Horizontal/LeftToRight.cs
@@ -0,0 +1,13 @@
+﻿namespace WordSearch.Services.GridDataPlacement.WordDirection.Horizontal
+{
+    using WordSearch.Services.GridDataPlacement.WordDirection.Interfaces;
+
+    public class LeftToRight : IWordDirection
+    {
+        public void MoveInGrid(ref int row, ref int column)
+        {
+            row += MovementState.Stay;
+            column += MovementState.ShiftOneCell;
+        }
+    }
+}
---
+++ src/Common/WordSearch.Services/GridDataPlacement/WordDirection/[Interfaces]/IWordDirection.cs
@@ -0,0 +1,14 @@
+﻿namespace WordSearch.Services.GridDataPlacement.WordDirection.Interfaces
+{
+    public interface IWordDirection
+    {
+        void MoveInGrid(ref int row, ref int column);
+    }
+
+    public struct MovementState
+    {
+        public const int Stay = 0;
+
+        public const int ShiftOneCell = 1;
+    }
+}

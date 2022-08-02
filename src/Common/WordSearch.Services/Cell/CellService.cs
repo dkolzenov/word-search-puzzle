5aef4ab7ad618c4e8df16a35e19847fbce2e3062
Donat Kolzenov <donat_kolzenov@mail.ru>
2022-08-02 17:16:59 +0900

    add: cell service

---
+++ src/Common/WordSearch.Services/Cell/CellService.cs
@@ -0,0 +1,27 @@
+﻿namespace WordSearch.Services.Cell
+{
+    using WordSearch.Services.Interfaces;
+    using WordSearch.Models.Cell;
+
+    public class CellService : ICellService
+    {
+        public CellModel[,] GetTwoDimensionalCellArray(int row, int column)
+        {
+            var cellArray = new CellModel[row, column];
+
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < column; j++)
+                {
+                    var cell = new CellModel()
+                    {
+                        Row = i + 1,
+                        Column = j + 1
+                    };
+                    cellArray[i, j] = cell;
+                }
+            }
+            return cellArray;
+        }
+    }
+}
---
+++ src/Common/WordSearch.Services/[Interfaces]/ICellService.cs
@@ -0,0 +1,9 @@
+﻿namespace WordSearch.Services.Interfaces
+{
+    using WordSearch.Models.Cell;
+
+    public interface ICellService
+    {
+        CellModel[,] GetTwoDimensionalCellArray(int row, int column);
+    }
+}

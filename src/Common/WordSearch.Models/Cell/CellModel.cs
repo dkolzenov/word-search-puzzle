69d2a8150c4e169a52124f74ac9149654d8bba42
Donat Kolzenov <donat_kolzenov@mail.ru>
2022-08-02 15:44:28 +0900

    add: cell model

---
+++ src/Common/WordSearch.Models/Cell/CellModel.cs
@@ -0,0 +1,13 @@
+﻿namespace WordSearch.Models.Cell
+{
+    public class CellModel
+    {
+        public int Id { get; set; }
+
+        public int Row { get; set; }
+
+        public int Column { get; set; }
+
+        public char Character { get; set; }
+    }
+}

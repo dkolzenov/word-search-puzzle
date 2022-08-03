e6a4dcdffbc7aa54d6207d074b04088597bb6303
Donat Kolzenov <donat_kolzenov@mail.ru>
2022-08-03 20:15:31 +0900

    add: direction service

---
+++ src/Common/WordSearch.Services/Direction/DirectionService.cs
@@ -0,0 +1,25 @@
+﻿namespace WordSearch.Services.Direction
+{
+    using System.Threading.Tasks;
+    using System.Collections.Generic;
+
+    using WordSearch.Services.Interfaces;
+    using WordSearch.Models.Direction;
+
+    public class DirectionService : IDirectionService
+    {
+        public DirectionService()
+        {
+        }
+
+        public Task<List<DirectionModel>> GetAllDirections()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<DirectionModel> GetDirection()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}

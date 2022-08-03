ead6fc8401db7c12f57bbb2ecd05887009ebfd31
Donat Kolzenov <donat_kolzenov@mail.ru>
2022-08-03 22:55:26 +0900

    add: layout to string map to direction service

---
+++ src/Common/WordSearch.Services/Direction/[Dictionaries]/LayoutToStringMap.cs
@@ -0,0 +1,26 @@
+﻿namespace WordSearch.Services.Direction.Dictionaries
+{
+    using System.Collections.Generic;
+
+    using WordSearch.Services.Direction.Enums;
+
+    public class LayoutToStringMap
+    {
+        private readonly Dictionary<LayoutType, string> _map;
+
+        public LayoutToStringMap()
+        {
+            _map = new Dictionary<LayoutType, string>()
+            {
+                {LayoutType.Vertical, "vertical"},
+                {LayoutType.Horizontal, "horizontal"},
+                {LayoutType.Diagonal, "diagonal"}
+            };
+        }
+
+        public string GetLayoutString(LayoutType layoutType)
+        {
+            return _map[layoutType];
+        }
+    }
+}

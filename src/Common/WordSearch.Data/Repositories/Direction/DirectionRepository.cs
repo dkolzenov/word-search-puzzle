3176f33fbff22228509bc4b717e6becc1fd1d05d
Donat Kolzenov <donat_kolzenov@mail.ru>
2022-08-03 22:15:16 +0900

    add: direction repository

---
+++ src/Common/WordSearch.Data/Repositories/Direction/DirectionRepository.cs
@@ -0,0 +1,15 @@
+﻿namespace WordSearch.Data.Repositories.Direction
+{
+    using WordSearch.Data.Repositories.Interfaces;
+    using WordSearch.Data.Repositories.Base;
+    using WordSearch.Data.Entities.Direction;
+    using WordSearch.Data.Contexts.ApplicationDb;
+
+    public class DirectionRepository
+        : RepositoryBase<DirectionEntity>, IDirectionRepository
+    {
+        public DirectionRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+    }
+}

6ae050bfde9bc4c12dd1fcda29bf2d916123650a
Donat Kolzenov <donat_kolzenov@mail.ru>
2022-08-04 23:42:59 +0900

    update: grid data service

---
+++ src/Common/WordSearch.Services/[Interfaces]/IGridDataService.cs
@@ -0,0 +1,12 @@
+﻿namespace WordSearch.Services.Interfaces
+{
+    using System.Threading.Tasks;
+
+    using WordSearch.Models.DataGrid;
+    using WordSearch.Models.GameSettings;
+
+    public interface IGridDataService
+    {
+        Task<GridDataModel> GetGridData(GameSettingsModel gameSettings);
+    }
+}

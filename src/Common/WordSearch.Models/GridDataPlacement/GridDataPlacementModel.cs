6f606e82733de4f6815d00096c73e7041b9e70eb
Donat Kolzenov <donat_kolzenov@mail.ru>
2022-08-04 18:52:10 +0900

    add: grid data placement model

---
+++ src/Common/WordSearch.Models/GridDataPlacement/GridDataPlacementModel.cs
@@ -0,0 +1,17 @@
+﻿namespace WordSearch.Models.GridDataPlacement
+{
+    using System.Collections.Generic;
+
+    using WordSearch.Models.Word;
+    using WordSearch.Models.Cell;
+    using WordSearch.Models.Character;
+
+    public class GridDataPlacementModel
+    {
+        public List<WordModel> Words { get; set; } = null!;
+
+        public List<CharacterModel> Characters { get; set; } = null!;
+
+        public List<CellModel> Cells { get; set; } = null!;
+    }
+}

3a7a4af998e59c1b6e5dd338b82c1f3165c93b95
Donat Kolzenov <donat_kolzenov@mail.ru>
2022-08-06 23:30:53 +0900

    add: character converter to mapper

---
+++ src/Common/WordSearch.Mappers/Converters/CharacterConverter.cs
@@ -0,0 +1,31 @@
+﻿namespace WordSearch.Mappers.Converters
+{
+    using System.Collections.Generic;
+
+    using AutoMapper;
+
+    using WordSearch.Models.Character;
+    using WordSearch.Data.Entities.Character;
+
+    public class CharacterConverter
+        : ITypeConverter<CharacterEntity, List<CharacterModel>>
+    {
+        public List<CharacterModel> Convert(
+            CharacterEntity source,
+            List<CharacterModel> destination,
+            ResolutionContext context)
+        {
+            char[] characters = source.Characters.ToCharArray();
+
+            foreach (var character in characters)
+            {
+                var characterModel = context.Mapper.Map<CharacterModel>(source);
+
+                characterModel.Value = character;
+
+                destination.Add(characterModel);
+            }
+            return destination;
+        }
+    }
+}

e9a764aac5404952bd66f117c7bac4dc3d5294cc
Donat Kolzenov <donat_kolzenov@mail.ru>
2022-08-06 23:22:33 +0900

    update: character model

--- src/Common/WordSearch.Models/Character/CharacterModel.cs
+++ src/Common/WordSearch.Models/Character/CharacterModel.cs
@@ -1,12 +1,10 @@
 ﻿namespace WordSearch.Models.Character
 {
-    using System.Collections.Generic;
-
     public class CharacterModel
     {
         public int Id { get; set; }
 
-        public List<char> Characters { get; set; } = null!;
+        public string Value { get; set; } = null!;
 
         public string Sctipt { get; set; } = null!;
     }

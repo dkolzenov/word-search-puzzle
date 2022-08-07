dbda215be6144395cebd8962d007bfc20745eebd
Donat Kolzenov <donat_kolzenov@mail.ru>
2022-08-07 15:53:07 +0900

    add: game settings dictionaries

---
+++ src/Common/WordSearch.Core/Dictionaries/GameSettings/DifficultyToStringMap.cs
@@ -0,0 +1,26 @@
+﻿namespace WordSearch.Core.Dictionaries.GameSettings
+{
+    using System.Collections.Generic;
+
+    using WordSearch.Core.Enums.GameSettings;
+
+    public class DifficultyToStringMap
+    {
+        private readonly Dictionary<DifficultyType, string> _map;
+
+        public DifficultyToStringMap()
+        {
+            _map = new Dictionary<DifficultyType, string>()
+            {
+                {DifficultyType.Easy, "easy"},
+                {DifficultyType.Normal, "normal"},
+                {DifficultyType.Hard, "hard"},
+            };
+        }
+
+        public string GetDifficultyString(DifficultyType difficultyType)
+        {
+            return _map[difficultyType];
+        }
+    }
+}

72e781b703bf056701b9a2851eb6f0e1dd9b88b2
Donat Kolzenov <donat_kolzenov@mail.ru>
2022-08-07 13:31:25 +0900

    add: grid dictionaries

---
+++ src/Common/WordSearch.Core/Dictionaries/Grid/SizeToStringMap.cs
@@ -0,0 +1,26 @@
+﻿namespace WordSearch.Core.Dictionaries.Grid
+{
+    using System.Collections.Generic;
+
+    using WordSearch.Core.Enums.Grid;
+
+    public class SizeToStringMap
+    {
+        private readonly Dictionary<SizeType, string> _map;
+
+        public SizeToStringMap()
+        {
+            _map = new Dictionary<SizeType, string>()
+            {
+                {SizeType.Small, "small"},
+                {SizeType.Medium, "medium"},
+                {SizeType.Large, "large"},
+            };
+        }
+
+        public string GetSizeString(SizeType sizeType)
+        {
+            return _map[sizeType];
+        }
+    }
+}

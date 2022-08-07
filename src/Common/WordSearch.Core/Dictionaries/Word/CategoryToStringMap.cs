20fefb8c89874c7ce85b094c92d55290f55ccc37
Donat Kolzenov <donat_kolzenov@mail.ru>
2022-08-07 13:07:20 +0900

    add: word dictionaries

---
+++ src/Common/WordSearch.Core/Dictionaries/Word/CategoryToStringMap.cs
@@ -0,0 +1,26 @@
+﻿namespace WordSearch.Core.Dictionaries.Word
+{
+    using System.Collections.Generic;
+
+    using WordSearch.Core.Enums.Word;
+
+    public class CategoryToStringMap
+    {
+        private readonly Dictionary<CategoryType, string> _map;
+
+        public CategoryToStringMap()
+        {
+            _map = new Dictionary<CategoryType, string>()
+            {
+                {CategoryType.Animals,    "animals"},
+                {CategoryType.Vegetables, "vegetables"},
+                {CategoryType.Fruits,     "fruits"}
+            };
+        }
+
+        public string GetCategoryString(CategoryType categoryType)
+        {
+            return _map[categoryType];
+        }
+    }
+}
---
+++ src/Common/WordSearch.Core/Dictionaries/Word/LanguageToStringMap.cs
@@ -0,0 +1,25 @@
+﻿namespace WordSearch.Core.Dictionaries.Word
+{
+    using System.Collections.Generic;
+
+    using WordSearch.Core.Enums.Word;
+
+    public class LanguageToStringMap
+    {
+        private readonly Dictionary<LanguageType, string> _map;
+
+        public LanguageToStringMap()
+        {
+            _map = new Dictionary<LanguageType, string>()
+            {
+                {LanguageType.Russian, "russian"},
+                {LanguageType.English, "english"}
+            };
+        }
+
+        public string GetLanguageString(LanguageType languageType)
+        {
+            return _map[languageType];
+        }
+    }
+}

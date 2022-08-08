085761ffcb8822f4003d7e9679ddc7957d5c1970
Donat Kolzenov <donat_kolzenov@mail.ru>
2022-08-09 01:28:23 +0900

    update: grid data model

--- src/Common/WordSearch.Models/GridData/GridDataModel.cs
+++ src/Common/WordSearch.Models/GridData/GridDataModel.cs
@@ -2,11 +2,14 @@
 {
     using System.Collections.Generic;
 
+    using WordSearch.Models.Grid;
     using WordSearch.Models.Word;
     using WordSearch.Models.Character;
 
     public class GridDataModel
     {
+        public GridModel Grid { get; set; } = null!;
+
         public List<WordModel> Words { get; set; } = null!;
 
         public List<CharacterModel> Characters { get; set; } = null!;

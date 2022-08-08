bcc4443f2f05a8c785162bec845596c76e1f2b46
Donat Kolzenov <donat_kolzenov@mail.ru>
2022-08-08 18:32:40 +0900

    update: character repository

--- src/Common/WordSearch.Data/Repositories/Character/CharacterRepository.cs
+++ src/Common/WordSearch.Data/Repositories/Character/CharacterRepository.cs
@@ -1,15 +1,44 @@
 ﻿namespace WordSearch.Data.Repositories.Character
 {
+    using System;
+    using System.Linq;
+    using System.Threading.Tasks;
+
     using WordSearch.Data.Repositories.Interfaces;
     using WordSearch.Data.Repositories.Base;
     using WordSearch.Data.Entities.Character;
     using WordSearch.Data.Contexts.ApplicationDb;
+    using WordSearch.Core.Enums.Character;
+    using WordSearch.Core.Dictionaries.Character;
 
     public class CharacterRepository
         : RepositoryBase<CharacterEntity>, ICharacterRepository
     {
+        private readonly ScriptToStringMap _scriptMap;
+
         public CharacterRepository(ApplicationDbContext context) : base(context)
         {
+            _scriptMap = new ScriptToStringMap();
+        }
+
+        public async Task<CharacterEntity> GetCharactersAsync(
+            ScriptType scriptType)
+        {
+            try
+            {
+                string script = _scriptMap.GetScriptString(scriptType);
+
+                var result = await QueryAsync(
+                    characters => characters.Script == script);
+
+                CharacterEntity characters = result.FirstOrDefault();
+
+                return characters;
+            }
+            catch (Exception ex)
+            {
+                return await Task.FromException<CharacterEntity>(ex);
+            }
         }
     }
 }
--- src/Common/WordSearch.Data/Repositories/[Interfaces]/ICharacterRepository.cs
+++ src/Common/WordSearch.Data/Repositories/[Interfaces]/ICharacterRepository.cs
@@ -1,8 +1,12 @@
 ﻿namespace WordSearch.Data.Repositories.Interfaces
 {
+    using System.Threading.Tasks;
+
     using WordSearch.Data.Entities.Character;
+    using WordSearch.Core.Enums.Character;
 
     public interface ICharacterRepository : IRepositoryBase<CharacterEntity>
     {
+        Task<CharacterEntity> GetCharactersAsync(ScriptType scriptType);
     }
 }

ccbd3b65a9cd2efb63206e6df8b2f49f4ba3b2cd
Donat Kolzenov <donat_kolzenov@mail.ru>
2022-08-09 02:14:39 +0900

    add: grid data placement service

---
+++ src/Common/WordSearch.Services/GridDataPlacement/GridDataPlacementService.cs
@@ -0,0 +1,55 @@
+﻿namespace WordSearch.Services.GridDataPlacement
+{
+    using System;
+    using System.Threading.Tasks;
+
+    using AutoMapper;
+
+    using WordSearch.Services.Interfaces;
+    using WordSearch.Helpers.Interfaces;
+    using WordSearch.Models.GridData;
+    using WordSearch.Models.GridDataPlacement;
+
+    public class GridDataPlacementService : IGridDataPlacementService
+    {
+        private readonly IMapper _mapper;
+
+        private readonly IDirectionService _directionService;
+
+        private readonly IRandomChooserHelper _randomChooserHelper;
+
+        public GridDataPlacementService(
+            IMapper mapper,
+            IDirectionService directionService,
+            IRandomChooserHelper randomChooserHelper)
+        {
+            _mapper = mapper;
+            _directionService = directionService;
+            _randomChooserHelper = randomChooserHelper;
+        }
+
+        public async Task<GridDataPlacementModel> GetGridDataPlacementAsync(
+            GridDataModel gridData)
+        {
+            try
+            {
+                var cells = gridData.Grid.Cells;
+
+                var randomWords = _randomChooserHelper
+                    .GetRandomUniqueList(gridData.Words, gridData.WordCount);
+
+                // TODO: temp plug
+                return new GridDataPlacementModel()
+                {
+                    Grid = gridData.Grid,
+                    RandomWords = randomWords
+                };
+            }
+            catch (Exception ex)
+            {
+                return await Task.FromException<GridDataPlacementModel>(
+                    ex.InnerException);
+            }
+        }
+    }
+}

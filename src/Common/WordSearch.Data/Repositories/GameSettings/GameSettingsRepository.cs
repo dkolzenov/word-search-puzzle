0a43bc815f62e0cb499063cb3e31b09376d983c5
Donat Kolzenov <donat_kolzenov@mail.ru>
2022-08-08 19:33:18 +0900

    update: game settings repository

--- src/Common/WordSearch.Data/Repositories/GameSettings/GameSettingsRepository.cs
+++ src/Common/WordSearch.Data/Repositories/GameSettings/GameSettingsRepository.cs
@@ -1,9 +1,14 @@
 ﻿namespace WordSearch.Data.Repositories.GameSettings
 {
+    using System;
+    using System.Linq;
+    using System.Threading.Tasks;
+
     using WordSearch.Data.Repositories.Interfaces;
     using WordSearch.Data.Repositories.Base;
     using WordSearch.Data.Entities.GameSettings;
     using WordSearch.Data.Contexts.ApplicationDb;
+    using WordSearch.Core.Enums.GameSettings;
 
     public class GameSettingsRepository
         : RepositoryBase<GameSettingsEntity>, IGameSettingsRepository
@@ -11,6 +16,27 @@
         public GameSettingsRepository(ApplicationDbContext context)
             : base(context)
         {
+        }
+
+        public async Task<GameSettingsEntity> GetGameSettings(
+            DifficultyType difficultyType)
+        {
+            try
+            {
+                string difficulty = difficultyType.ToString().ToLower();
+
+                var result = await QueryAsync(
+                    settings => settings.Difficulty == difficulty);
+
+                GameSettingsEntity gameSettings = result.FirstOrDefault();
+
+                return gameSettings;
+            }
+            catch (Exception ex)
+            {
+                return await Task.FromException<GameSettingsEntity>(
+                    ex.InnerException);
+            }
         }
     }
 }
--- src/Common/WordSearch.Data/Repositories/[Interfaces]/IGameSettingsRepository.cs
+++ src/Common/WordSearch.Data/Repositories/[Interfaces]/IGameSettingsRepository.cs
@@ -1,8 +1,12 @@
 ﻿namespace WordSearch.Data.Repositories.Interfaces
 {
+    using System.Threading.Tasks;
+
     using WordSearch.Data.Entities.GameSettings;
+    using WordSearch.Core.Enums.GameSettings;
 
     public interface IGameSettingsRepository : IRepositoryBase<GameSettingsEntity>
     {
+        Task<GameSettingsEntity> GetGameSettings(DifficultyType difficultyType);
     }
 }

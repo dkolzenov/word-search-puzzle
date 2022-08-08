c392c7d7b7b0b30780d6c9fcf27e9a2ce82ce3f0
Donat Kolzenov <donat_kolzenov@mail.ru>
2022-08-08 19:49:15 +0900

    update: word repository

--- src/Common/WordSearch.Data/Repositories/Word/WordRepository.cs
+++ src/Common/WordSearch.Data/Repositories/Word/WordRepository.cs
@@ -1,14 +1,42 @@
 ﻿namespace WordSearch.Data.Repositories.Word
 {
+    using System;
+    using System.Linq;
+    using System.Threading.Tasks;
+
     using WordSearch.Data.Repositories.Interfaces;
     using WordSearch.Data.Repositories.Base;
     using WordSearch.Data.Entities.Word;
     using WordSearch.Data.Contexts.ApplicationDb;
+    using WordSearch.Core.Enums.Word;
 
     public class WordRepository : RepositoryBase<WordEntity>, IWordRepository
     {
         public WordRepository(ApplicationDbContext context) : base(context)
         {
+        }
+
+        public async Task<WordEntity> GetWordsAsync(
+            LanguageType languageType,
+            CategoryType categoryType)
+        {
+            try
+            {
+                string language = languageType.ToString().ToLower();
+                string category = categoryType.ToString().ToLower();
+
+                var result = await QueryAsync(
+                    word => word.Language == language,
+                    word => word.Category == category);
+
+                WordEntity words = result.FirstOrDefault();
+
+                return words;
+            }
+            catch (Exception ex)
+            {
+                return await Task.FromException<WordEntity>(ex.InnerException);
+            }
         }
     }
 }

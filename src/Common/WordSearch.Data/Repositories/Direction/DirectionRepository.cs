21ab8bb8017e2fb4ec00b54c3d886f1a2a50682a
Donat Kolzenov <donat_kolzenov@mail.ru>
2022-08-08 18:51:30 +0900

    update: direction repository

--- src/Common/WordSearch.Data/Repositories/Direction/DirectionRepository.cs
+++ src/Common/WordSearch.Data/Repositories/Direction/DirectionRepository.cs
@@ -1,15 +1,39 @@
 ﻿namespace WordSearch.Data.Repositories.Direction
 {
+    using System;
+    using System.Threading.Tasks;
+    using System.Collections.Generic;
+
     using WordSearch.Data.Repositories.Interfaces;
     using WordSearch.Data.Repositories.Base;
     using WordSearch.Data.Entities.Direction;
     using WordSearch.Data.Contexts.ApplicationDb;
+    using WordSearch.Core.Enums.Direction;
 
     public class DirectionRepository
         : RepositoryBase<DirectionEntity>, IDirectionRepository
     {
         public DirectionRepository(ApplicationDbContext context) : base(context)
         {
+        }
+
+        public async Task<List<DirectionEntity>> GetDirectionsAsync(
+            LayoutType layoutType)
+        {
+            try
+            {
+                string layout = layoutType.ToString().ToLower();
+
+                var directions = await QueryAsync(
+                    direction => direction.Layout == layout);
+
+                return directions;
+            }
+            catch (Exception ex)
+            {
+                return await Task.FromException<List<DirectionEntity>>(
+                    ex.InnerException);
+            }
         }
     }
 }
--- src/Common/WordSearch.Data/Repositories/[Interfaces]/IDirectionRepository.cs
+++ src/Common/WordSearch.Data/Repositories/[Interfaces]/IDirectionRepository.cs
@@ -1,8 +1,13 @@
 ﻿namespace WordSearch.Data.Repositories.Interfaces
 {
+    using System.Threading.Tasks;
+    using System.Collections.Generic;
+
     using WordSearch.Data.Entities.Direction;
+    using WordSearch.Core.Enums.Direction;
 
     public interface IDirectionRepository : IRepositoryBase<DirectionEntity>
     {
+        Task<List<DirectionEntity>> GetDirectionsAsync(LayoutType layoutType);
     }
 }

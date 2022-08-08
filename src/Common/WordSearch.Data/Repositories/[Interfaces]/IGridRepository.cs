8faa362e64ba5c3b8df9752908db1b1b1c40e318
Donat Kolzenov <donat_kolzenov@mail.ru>
2022-08-08 19:40:19 +0900

    update: grid repository

--- src/Common/WordSearch.Data/Repositories/[Interfaces]/IGridRepository.cs
+++ src/Common/WordSearch.Data/Repositories/[Interfaces]/IGridRepository.cs
@@ -1,8 +1,12 @@
 ﻿namespace WordSearch.Data.Repositories.Interfaces
 {
+    using System.Threading.Tasks;
+
     using WordSearch.Data.Entities.Grid;
+    using WordSearch.Core.Enums.Grid;
 
     public interface IGridRepository : IRepositoryBase<GridEntity>
     {
+        Task<GridEntity> GetGridAsync(SizeType sizeType);
     }
 }

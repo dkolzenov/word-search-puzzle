eaf45656123b9ce284066896d2b6e0b17db74c30
Donat Kolzenov <donat_kolzenov@mail.ru>
2022-08-09 22:31:47 +0900

    add: character insert service

---
+++ src/Common/WordSearch.Services/CharacterInsert/CharacterInsertService.cs
@@ -0,0 +1,48 @@
+﻿namespace WordSearch.Services.CharacterInsert
+{
+    using System;
+    using System.Threading.Tasks;
+    using System.Collections.Generic;
+
+    using WordSearch.Services.Interfaces;
+    using WordSearch.Helpers.Interfaces;
+    using WordSearch.Models.Grid;
+    using WordSearch.Models.Character;
+
+    public class CharacterInsertService : ICharacterInsertService
+    {
+        private readonly IRandomChooserHelper _randomChooserHelper;
+
+        public CharacterInsertService(IRandomChooserHelper randomChooserHelper)
+        {
+            _randomChooserHelper = randomChooserHelper;
+        }
+
+        public async Task<GridModel> GetCharacterInsertedGrid(
+            GridModel grid,
+            List<CharacterModel> characters)
+        {
+            try
+            {
+                for (int i = 0; i < grid.Row; i++)
+                {
+                    for (int j = 0; j < grid.Column; j++)
+                    {
+                        if (grid[i, j].Character is null)
+                        {
+                            var randomCharacter = _randomChooserHelper
+                                .GetRandomItem(characters);
+
+                            grid[i, j].Character = randomCharacter;
+                        }
+                    }
+                }
+                return grid;
+            }
+            catch (Exception ex)
+            {
+                return await Task.FromException<GridModel>(ex.InnerException);
+            }
+        }
+    }
+}

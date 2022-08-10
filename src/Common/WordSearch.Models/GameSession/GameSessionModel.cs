f1f01b308d22f577de22375cf90dca1af9783930
Donat Kolzenov <donat_kolzenov@mail.ru>
2022-08-10 18:57:24 +0900

    update: game session model

--- src/Common/WordSearch.Models/GameSession/GameSessionModel.cs
+++ src/Common/WordSearch.Models/GameSession/GameSessionModel.cs
@@ -11,7 +11,7 @@
     {
         public Guid Id { get; set; }
 
-        public GridModel Grid { get; set; } = null!;
+        public GridModel DataGrid { get; set; } = null!;
 
         public List<WordModel> Words { get; set; } = null!;
 

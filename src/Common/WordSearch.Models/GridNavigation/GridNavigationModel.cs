51bbfe8d261addb632251c6709f0b54c52e416f3
Donat Kolzenov <donat_kolzenov@mail.ru>
2022-08-10 18:48:34 +0900

    add: grid navigation model

---
+++ src/Common/WordSearch.Models/GridNavigation/GridNavigationModel.cs
@@ -0,0 +1,15 @@
+﻿namespace WordSearch.Models.GridNavigation
+{
+    using WordSearch.Models.Grid;
+    using WordSearch.Models.Cell;
+    using WordSearch.Models.Direction;
+
+    public class GridNavigationModel
+    {
+        public GridModel Grid { get; set; } = null!;
+
+        public CellModel StartPointCell { get; set; } = null!;
+
+        public DirectionModel Direction { get; set; } = null!;
+    }
+}

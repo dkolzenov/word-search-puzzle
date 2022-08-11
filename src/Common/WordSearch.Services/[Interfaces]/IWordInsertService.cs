a7e63aa59132be13ca5991092bf50868fc51caba
Donat Kolzenov <donat_kolzenov@mail.ru>
2022-08-11 21:53:33 +0900

    add: word insert service

---
+++ src/Common/WordSearch.Services/[Interfaces]/IWordInsertService.cs
@@ -0,0 +1,12 @@
+﻿namespace WordSearch.Services.Interfaces
+{
+    using System.Threading.Tasks;
+
+    using WordSearch.Models.Grid;
+    using WordSearch.Models.Word;
+
+    public interface IWordInsertService
+    {
+        Task<GridModel> GetWordInsertedGrid(GridModel grid, WordModel word);
+    }
+}

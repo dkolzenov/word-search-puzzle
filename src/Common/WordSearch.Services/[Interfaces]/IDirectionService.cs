a34e0c0d01469836c53fa2b4d1ae34f10fdbf306
Donat Kolzenov <donat_kolzenov@mail.ru>
2022-08-15 23:48:30 +0900

    update: direction service

--- src/Common/WordSearch.Services/[Interfaces]/IDirectionService.cs
+++ src/Common/WordSearch.Services/[Interfaces]/IDirectionService.cs
@@ -4,10 +4,18 @@
     using System.Collections.Generic;
 
     using WordSearch.Models.Direction;
+    using WordSearch.Models.Grid;
+    using WordSearch.Models.Word;
+    using WordSearch.Models.Cell;
     using WordSearch.Core.Enums.Direction;
 
     public interface IDirectionService
     {
         Task<List<DirectionModel>> GetDirectionsAsync(LayoutType layoutType);
+
+        Task<DirectionModel?> GetValidRandomWordDirectionAsync(
+            GridModel grid,
+            WordModel word,
+            CellModel startCell);
     }
 }

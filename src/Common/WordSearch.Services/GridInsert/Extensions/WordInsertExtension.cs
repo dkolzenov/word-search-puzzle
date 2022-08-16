7e5a41a915d6a6edd0e25ae27c26159d245fac8e
Donat Kolzenov <donat_kolzenov@mail.ru>
2022-08-16 23:51:52 +0900

    add: grid insert service

---
+++ src/Common/WordSearch.Services/GridInsert/Extensions/WordInsertExtension.cs
@@ -0,0 +1,15 @@
+﻿namespace WordSearch.Services.GridInsert.Extensions
+{
+    using System.Threading.Tasks;
+
+    using WordSearch.Models.Grid;
+    using WordSearch.Models.Word;
+
+    public static class WordInsertExtension
+    {
+        public static Task InsertWord(this GridModel grid, WordModel word)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ src/Common/WordSearch.Services/[Interfaces]/IGridInsertService.cs
@@ -0,0 +1,14 @@
+﻿namespace WordSearch.Services.Interfaces
+{
+    using System.Threading.Tasks;
+
+    using WordSearch.Models.Grid;
+    using WordSearch.Models.GridData;
+
+    public interface IGridInsertService
+    {
+        Task<GridModel> GetDataInsertedGridAsync(
+            GridModel grid,
+            GridDataModel gridData);
+    }
+}

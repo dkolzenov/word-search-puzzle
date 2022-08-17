4a7b82a2ffc757150746d295e969b52204f93767
Donat Kolzenov <donat_kolzenov@mail.ru>
2022-08-17 21:20:51 +0900

    add: data grid model

---
+++ src/Common/WordSearch.Models/DataGrid/DataGridModel.cs
@@ -0,0 +1,14 @@
+﻿namespace WordSearch.Models.DataGrid
+{
+    using System.Collections.Generic;
+
+    using WordSearch.Models.Grid;
+    using WordSearch.Models.Word;
+
+    public class DataGridModel
+    {
+        public GridModel Grid { get; set; } = null!;
+
+        public List<WordModel> Words { get; set; } = null!;
+    }
+}

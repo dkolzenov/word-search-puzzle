3a611d56203716053856398b628515caa49c6644
Donat Kolzenov <donat_kolzenov@mail.ru>
2022-08-19 23:16:11 +0900

    add: data grid service

---
+++ src/Common/WordSearch.Services/DataGrid/DataGridService.cs
@@ -0,0 +1,33 @@
+﻿namespace WordSearch.Services.DataGrid
+{
+    using System;
+    using System.Threading.Tasks;
+
+    using WordSearch.Services.Interfaces;
+    using WordSearch.Models.DataGrid;
+    using WordSearch.Models.GridData;
+    using WordSearch.Models.Grid;
+
+    public class DataGridService : IDataGridService
+    {
+        public DataGridService()
+        {
+
+        }
+
+        public async Task<DataGridModel> GetDataGridAsync(
+            GridModel grid,
+            GridDataModel gridData)
+        {
+            try
+            {
+                throw new NotImplementedException();
+            }
+            catch (Exception ex)
+            {
+                return await Task.FromException<DataGridModel>(
+                    ex.InnerException);
+            }
+        }
+    }
+}
---
+++ src/Common/WordSearch.Services/[Interfaces]/IDataGridService.cs
@@ -0,0 +1,15 @@
+﻿namespace WordSearch.Services.Interfaces
+{
+    using System.Threading.Tasks;
+
+    using WordSearch.Models.DataGrid;
+    using WordSearch.Models.GridData;
+    using WordSearch.Models.Grid;
+
+    public interface IDataGridService
+    {
+        Task<DataGridModel> GetDataGridAsync(
+            GridModel grid,
+            GridDataModel gridData);
+    }
+}

cf766cbbefab594377526b0561402e230b09c0ff
Donat Kolzenov <donat_kolzenov@mail.ru>
2022-08-30 22:48:36 +0700

    add: word on grid model

---
+++ src/Common/WordSearch.Models/WordOnGrid/WordOnGridModel.cs
@@ -0,0 +1,15 @@
+﻿namespace WordSearch.Models.WordOnGrid
+{
+    using WordSearch.Models.Word;
+    using WordSearch.Models.Cell;
+    using WordSearch.Models.Direction;
+
+    public class WordOnGridModel
+    {
+        public WordModel Word { get; set; } = null!;
+
+        public CellModel StartCell { get; set; } = null!;
+
+        public DirectionModel Direction { get; set; } = null!;
+    }
+}

c8bca23d5eea5baf55bc192314843c39b3cb4579
Donat Kolzenov <donat_kolzenov@mail.ru>
2022-09-02 23:13:21 +0700

    add: word on grid entity

---
+++ src/Common/WordSearch.Data/Entities/WordOnGrid/WordOnGridEntity.cs
@@ -0,0 +1,14 @@
+﻿namespace WordSearch.Data.Entities.WordOnGrid
+{
+    using WordSearch.Data.Entities.Word;
+    using WordSearch.Data.Entities.Direction;
+
+    public class WordOnGridEntity
+    {
+        public int Id { get; set; }
+
+        public WordEntity Word { get; set; } = null!;
+
+        public DirectionEntity Direction { get; set; } = null!;
+    }
+}

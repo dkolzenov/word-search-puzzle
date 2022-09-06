7b62e8cc1341be186ee94399d48f7ce88155d631
Donat Kolzenov <donat_kolzenov@mail.ru>
2022-09-06 15:19:49 +0700

    add: word on grid repository

---
+++ src/Common/WordSearch.Data/Repositories/WordOnGrid/WordOnGridRepository.cs
@@ -0,0 +1,16 @@
+﻿namespace WordSearch.Data.Repositories.WordOnGrid
+{
+    using WordSearch.Data.Repositories.Interfaces;
+    using WordSearch.Data.Repositories.Base;
+    using WordSearch.Data.Entities.WordOnGrid;
+    using WordSearch.Data.Contexts.ApplicationDb;
+
+    public class WordOnGridRepository
+        : RepositoryBase<WordOnGridEntity>, IWordOnGridRepository
+    {
+        public WordOnGridRepository(ApplicationDbContext context)
+            : base(context)
+        {
+        }
+    }
+}
---
+++ src/Common/WordSearch.Data/Repositories/[Interfaces]/IWordOnGridRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace WordSearch.Data.Repositories.Interfaces
+{
+    using WordSearch.Data.Entities.WordOnGrid;
+
+    public interface IWordOnGridRepository : IRepositoryBase<WordOnGridEntity>
+    {
+    }
+}

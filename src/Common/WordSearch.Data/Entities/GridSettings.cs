c98dc66cbb23aede19ed441b9571561bbbb53b3a
Donat Kolzenov <donat_kolzenov@mail.ru>
2022-11-30 23:53:15 +0700

    add: grid settings

---
+++ src/Common/WordSearch.Data/Entities/GridSettings.cs
@@ -0,0 +1,11 @@
+namespace WordSearch.Data.Entities
+{
+    public class GridSettings
+    {
+        public int RowCount { get; set; }
+        
+        public int ColumnCount { get; set; }
+        
+        public string Size { get; set; }
+    }
+}

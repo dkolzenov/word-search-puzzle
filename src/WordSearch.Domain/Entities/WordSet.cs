ef485ff30af23cc7e9bca4e4b893441b8f0fbfb5
Donat Kolzenov <donat_kolzenov@mail.ru>
2022-12-05 03:54:32 +0700

    add: word set entity

---
+++ src/WordSearch.Domain/Entities/WordSet.cs
@@ -0,0 +1,28 @@
+using WordSearch.Domain.Interfaces;
+
+namespace WordSearch.Domain.Entities
+{
+    /// <summary>
+    /// Список слов
+    /// </summary>
+    public class WordSet : IBaseEntity
+    {
+        /// <inheritdoc />
+        public long Id { get; set; }
+
+        /// <summary>
+        /// Слова
+        /// </summary>
+        public string Words { get; set; } = null!;
+
+        /// <summary>
+        /// Категория
+        /// </summary>
+        public string Category { get; set; } = null!;
+        
+        /// <summary>
+        /// Длина слова
+        /// </summary>
+        public int WordLength { get; set; }
+    }
+}

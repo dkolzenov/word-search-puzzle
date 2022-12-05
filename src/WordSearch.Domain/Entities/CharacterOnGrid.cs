48e26ec563ab5dd4b9cf6a6f0f6dff1beb843fc4
Donat Kolzenov <donat_kolzenov@mail.ru>
2022-12-05 22:26:00 +0700

    add: character on grid

---
+++ src/WordSearch.Domain/Entities/CharacterOnGrid.cs
@@ -0,0 +1,58 @@
+using WordSearch.Domain.Interfaces;
+
+namespace WordSearch.Domain.Entities
+{
+    /// <summary>
+    /// Буква в сетке
+    /// </summary>
+    public class CharacterOnGrid : IBaseEntity
+    {
+        /// <inheritdoc />
+        public long Id { get; set; }
+
+        /// <summary>
+        /// Буква
+        /// </summary>
+        public string Character { get; set; } = null!;
+        
+        /// <summary>
+        /// Строка
+        /// </summary>
+        public int Row { get; set; }
+        
+        /// <summary>
+        /// Столбец
+        /// </summary>
+        public int Column { get; set; }
+        
+        /// <summary>
+        /// Идентификатор алфавита
+        /// </summary>
+        public long AlphabetId { get; set; }
+
+        /// <summary>
+        /// Алфавит
+        /// </summary>
+        public Alphabet Alphabet { get; set; } = null!;
+        
+        /// <summary>
+        /// Идентификатор слова в сетке
+        /// </summary>
+        public long WordOnGridId { get; set; }
+
+        /// <summary>
+        /// Слово в сетке
+        /// </summary>
+        public WordOnGrid WordOnGrid { get; set; } = null!;
+        
+        /// <summary>
+        /// Идентификатор игровой сессии
+        /// </summary>
+        public long GameSessionId { get; set; }
+
+        /// <summary>
+        /// Игровая сессия
+        /// </summary>
+        public GameSession GameSession { get; set; } = null!;
+    }
+}

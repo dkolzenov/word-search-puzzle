42c0cfc5410fe189f686a5f551bea749327e8699
Donat Kolzenov <donat_kolzenov@mail.ru>
2022-12-05 22:02:08 +0700

    update: layout type

--- src/WordSearch.Common/Enums/LayoutType.cs
+++ src/WordSearch.Common/Enums/LayoutType.cs
@@ -1,9 +1,23 @@
 namespace WordSearch.Common.Enums
 {
+    /// <summary>
+    /// Тип расположения
+    /// </summary>
     public enum LayoutType
     {
+        /// <summary>
+        /// Вертикальный
+        /// </summary>
         Vertical,
+        
+        /// <summary>
+        /// Горизонтальный
+        /// </summary>
         Horizontal,
+        
+        /// <summary>
+        /// Диагональный
+        /// </summary>
         Diagonal
     }
 }

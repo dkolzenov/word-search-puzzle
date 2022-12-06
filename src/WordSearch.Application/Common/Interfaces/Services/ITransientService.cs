6ed9b2f93b9b09d3d487eb4521889361e2b5bfca
Donat Kolzenov <donat_kolzenov@mail.ru>
2022-12-06 23:58:35 +0700

    add: transient service interface

---
+++ src/WordSearch.Application/Common/Interfaces/Services/ITransientService.cs
@@ -0,0 +1,9 @@
+namespace WordSearch.Application.Common.Interfaces.Services
+{
+    /// <summary>
+    /// Интерфейс-маркер для автоматической регистрации transient сервисов
+    /// </summary>
+    public interface ITransientService
+    {
+    }
+}

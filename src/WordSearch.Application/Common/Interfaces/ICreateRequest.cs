a214d68b04e10a7a49d8e0ceb847142360ba32b6
Donat Kolzenov <donat_kolzenov@mail.ru>
2022-12-07 02:47:35 +0700

    add: create request interface

---
+++ src/WordSearch.Application/Common/Interfaces/ICreateRequest.cs
@@ -0,0 +1,9 @@
+namespace WordSearch.Application.Common.Interfaces
+{
+    /// <summary>
+    /// Интерфейс-маркер тела запроса Create
+    /// </summary>
+    public interface ICreateRequest
+    {
+    }
+}

4fd097cd533f87b253f021bafa04c99ce0081f04
Donat Kolzenov <donat_kolzenov@mail.ru>
2022-12-07 04:30:35 +0700

    remove: services registration extension to infrastructure

--- src/WordSearch.Infrastructure/Extensions/ServicesRegistration.cs
+++ src/WordSearch.Infrastructure/Extensions/ServicesRegistration.cs
@@ -5,7 +5,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Prism.Ioc;
 using WordSearch.Application.Common.Interfaces;
 
-namespace WordSearch.Application.Extensions
+namespace WordSearch.Infrastructure.Extensions
 {
     public static class ServicesRegistration
     {

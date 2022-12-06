5dd4b30effff55c3caecc0877b37347b84366bc5
Donat Kolzenov <donat_kolzenov@mail.ru>
2022-12-06 23:55:55 +0700

    add: singleton service interface

---
+++ src/WordSearch.Application/Common/Interfaces/Services/ISingletonService.cs
@@ -0,0 +1,9 @@
+namespace WordSearch.Application.Common.Interfaces.Services
+{
+    /// <summary>
+    /// Интерфейс-маркер для автоматической регистрации singleton сервисов
+    /// </summary>
+    public interface ISingletonService
+    {
+    }
+}

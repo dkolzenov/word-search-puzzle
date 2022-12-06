337e46f52349365a36acffd0d65fb5eefe34453b
Donat Kolzenov <donat_kolzenov@mail.ru>
2022-12-06 23:57:09 +0700

    add: scoped service interface

---
+++ src/WordSearch.Application/Common/Interfaces/Services/IScopedService.cs
@@ -0,0 +1,9 @@
+namespace WordSearch.Application.Common.Interfaces.Services
+{
+    /// <summary>
+    /// Интерфейс-маркер для автоматической регистрации scoped сервисов
+    /// </summary>
+    public interface IScopedService
+    {
+    }
+}

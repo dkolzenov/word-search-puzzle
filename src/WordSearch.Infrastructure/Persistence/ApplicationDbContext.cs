9ac632752d06e1934bed9ed9e92ec30d8d8cfc49
Donat Kolzenov <donat_kolzenov@mail.ru>
2022-12-08 22:55:54 +0700

    add: alphabet entity to application db context

--- src/WordSearch.Infrastructure/Persistence/ApplicationDbContext.cs
+++ src/WordSearch.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -1,11 +1,14 @@
 using System.Reflection;
 using Microsoft.EntityFrameworkCore;
 using WordSearch.Application.Common.Interfaces;
+using WordSearch.Domain.Entities;
 
 namespace WordSearch.Infrastructure.Persistence
 {
     public class ApplicationDbContext : DbContext, IApplicationDbContext
     {
+        public DbSet<Alphabet> Alphabets { get; set; }
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
         }

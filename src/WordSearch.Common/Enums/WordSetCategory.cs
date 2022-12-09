cac1a392150229a04c602435a1c2de63f439106b
Donat Kolzenov <donat_kolzenov@mail.ru>
2022-12-10 00:42:22 +0700

    add: wordset category enum

---
+++ src/WordSearch.Common/Enums/WordSetCategory.cs
@@ -0,0 +1,23 @@
+namespace WordSearch.Common.Enums
+{
+    /// <summary>
+    /// Категория набора слов
+    /// </summary>
+    public enum WordSetCategory
+    {
+        /// <summary>
+        /// Животные
+        /// </summary>
+        Animals,
+        
+        /// <summary>
+        /// Фрукты
+        /// </summary>
+        Fruits,
+        
+        /// <summary>
+        /// Овощи
+        /// </summary>
+        Vegetables
+    }
+}

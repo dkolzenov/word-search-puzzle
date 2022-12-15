0f723188d7fdb8699782d1da5101f27a39d3f76c
Donat Kolzenov <donat_kolzenov@mail.ru>
2022-12-16 04:50:50 +0700

    update: infrastructure dependency injection

--- src/WordSearch.Infrastructure/DependencyInjection.cs
+++ src/WordSearch.Infrastructure/DependencyInjection.cs
@@ -1,8 +1,5 @@
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
 using Prism.Ioc;
-using WordSearch.Application.Common.Interfaces;
 using WordSearch.Infrastructure.Extensions;
 using WordSearch.Infrastructure.Persistence;
 
@@ -10,23 +7,12 @@ namespace WordSearch.Infrastructure
 {
     public static class DependencyInjection
     {
-        public static IContainerRegistry AddInfrastructure(this IContainerRegistry containerRegistry,
-            IConfiguration configuration)
-        {
-            InitializeDb(containerRegistry, configuration);
-
-            return containerRegistry;
-        }
-
-        private static void InitializeDb(this IContainerRegistry containerRegistry, IConfiguration configuration)
+        public static IContainerRegistry AddInfrastructure(this IContainerRegistry containerRegistry, IConfiguration configuration)
         {
             containerRegistry.AddSqliteContext<ApplicationDbContext>(configuration);
             containerRegistry.AddServicesRegistration();
-
-            containerRegistry.RegisterServices(services =>
-                services.BuildServiceProvider()
-                    .GetService<IApplicationDbContext>()!
-                    .Database.Migrate());
+            
+            return containerRegistry;
         }
     }
 }

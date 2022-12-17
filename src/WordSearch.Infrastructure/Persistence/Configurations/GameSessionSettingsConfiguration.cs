bfc91b85f1221cb70a4946a114cd9011f37ab5c0
Donat Kolzenov <donat_kolzenov@mail.ru>
2022-12-18 04:34:37 +0700

    add: game session settings configuration

---
+++ src/WordSearch.Infrastructure/Persistence/Configurations/GameSessionSettingsConfiguration.cs
@@ -0,0 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using WordSearch.Domain.Entities;
+
+namespace WordSearch.Infrastructure.Persistence.Configurations
+{
+    public class GameSessionSettingsConfiguration : IEntityTypeConfiguration<GameSessionSettings>
+    {
+        public void Configure(EntityTypeBuilder<GameSessionSettings> builder)
+        {
+            builder.Property(x => x.Id).ValueGeneratedOnAdd();
+            builder.Property(x => x.Difficulty).IsRequired();
+
+            builder.HasOne(x => x.GridSettings)
+                .WithMany()
+                .HasForeignKey(x => x.GridSettingsId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}

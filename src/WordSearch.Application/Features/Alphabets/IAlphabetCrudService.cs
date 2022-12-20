d17ea0c6be9854421f5e21cb4506552b74a664d3
Donat Kolzenov <donat_kolzenov@mail.ru>
2022-12-20 22:46:12 +0700

    update: alphabet crud service interface

--- src/WordSearch.Application/Features/Alphabets/IAlphabetCrudService.cs
+++ src/WordSearch.Application/Features/Alphabets/IAlphabetCrudService.cs
@@ -1,4 +1,6 @@
 using WordSearch.Application.Common.Interfaces;
+using WordSearch.Application.Features.Alphabets.Dto;
+using WordSearch.Application.Features.Alphabets.Requests;
 
 namespace WordSearch.Application.Features.Alphabets
 {
@@ -7,6 +9,12 @@ namespace WordSearch.Application.Features.Alphabets
     /// </summary>
     public interface IAlphabetCrudService : ISingletonService
     {
-        
+        /// <summary>
+        /// Получение списка алфавитов
+        /// </summary>
+        /// <param name="request">Тело запроса</param>
+        /// <param name="cancellationToken">Токен отмены</param>
+        /// <returns>Список алфавитов</returns>
+        Task<IList<AlphabetDto>> GetAlphabets(GetAlphabetsRequest request, CancellationToken cancellationToken = default);
     }
 }
